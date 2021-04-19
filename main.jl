4967057bbfba21f0ad2f72c34353622d5a9a7336
seongtaek.hong <seongtaek.hong@looxidlabs.com>
2021-04-19 22:29:25 +0900

    Refactor display data functions

--- main.jl
+++ main.jl
@@ -2,44 +2,37 @@ using CSV
 
 
 FILE_CONSTRUCTORS = "./resources/f1_data_set/constructors.csv" 
-FILE_DRIVERS = "./resources/f1_data_set/drivers.csv" 
+FILE_DRIVERS = "./resources/f1_data_set/drivers.csv"
+FILE_PIT_STOP_TIME = "./resources/f1_data_set/pit_stops.csv" 
+
+TARGET_FILE_DICT = Dict(
+    1=>FILE_CONSTRUCTORS,
+    2=>FILE_DRIVERS,
+    3=>FILE_PIT_STOP_TIME,
+)
 
 function main()
     user_input = print_main()
-    run_user_input(user_input)
+    display_data(TARGET_FILE_DICT[user_input])
 end
 
 function print_main()
     println("Let's race with the F1 data.")
-    println("0: Display Constructors")
-    println("1: Display Drivers")
+    println("1: Display Constructors")
+    println("2: Display Drivers")
+    println("3: Display Pit Stop Times")
+
     print("\nYour choice: ")
     user_choice = readline()
 
     return parse(Int64, user_choice)
 end
 
-function run_user_input(user_input::Int64)
-    if user_input == 0
-        display_constructors()
-    elseif user_input == 1
-        display_drivers()
-    end
-end
-
-function display_constructors()
-    obj_constructor = CSV.File(FILE_CONSTRUCTORS)
-    for row in obj_constructor
+function display_data(file)
+    obj_file = CSV.File(file)
+    for row in obj_file
         print(row)
     end
 end
-
-function display_drivers()
-    obj_drivers = CSV.File(FILE_DRIVERS)
-    for row in obj_drivers
-        print(row)
-    end
-end
-
 
 main()
